8f5f3d335b53526a598a1c567f6451d29d5787e4
Jorge Mejia <jorgemejia2501@gmail.com>
2025-01-09 11:37:47 -0600

    music provided

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
-import React from "react";
 import Header from "../components/organisms/Header";
+import React from "react";
 import Slider from "@/components/organisms/Slider";
 
 const Page = () => {
   return (
     <>
       <Header />
+
       <Slider />
     </>
   );
