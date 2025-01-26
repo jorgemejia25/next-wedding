6d24709ec61f6a027930e9ce9989445eba1a7dc3
Jorge Mejia <jorgemejia2501@gmail.com>
2025-01-26 17:36:49 -0600

    fix url

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
+import React, { Suspense } from "react";
+
 import CTA from "@/components/organisms/CTA"; // Importa el nuevo CTA
 import Countdown from "@/components/organisms/Countdown"; // Importa el nuevo Countdown
 import Header from "../components/organisms/Header";
 import Intro from "@/components/organisms/Intro";
-import React from "react";
 import Schedule from "@/components/organisms/Schedule";
 import Story from "@/components/organisms/Story";
 
 const Page = () => {
   return (
-    <>
+    <Suspense fallback={<div>Loading...</div>}>
       <Intro />
       <Header />
-
       <Schedule />
       <Story />
       <CTA />
-    </>
+    </Suspense>
   );
 };
 
